89ccb61d658f7fcb40f663cb0228280a6611d57f
Sushant Naik <sushantmnaik2008@gmail.com>
2026-07-22 13:49:20 +0530

    nav

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,27 +8,27 @@ const Footer = () => {
   var dataset = Object.values(data);
   return (
     <footer className="text-gray-400 bg-gray-900 body-font">
-  <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+  <div className=" px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
     <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
-      <Image id="logo" className="flex title-font font-medium items-center text-blue-300 mb-4 md:mb-0 md:w-20" src="/logo.png" alt="Logo" width={100} height={50} priority style={{ width: 'auto', height: 'auto' }} />
+      <Image id="logo" className="flex title-font font-medium items-center text-blue-300 mb-4 md:mb-0 md:w-20" src="/logo.png" alt="Logo" width={80} height={80} priority style={{ width: 'auto', height: 'auto' }} />
       <span className="ml-3 text-xl">
-      <Image id="title" src="/waideek.png" alt="Waideek" width={200} height={100} priority style={{ width: 'auto', height: 'auto' }} />
+      <Image id="title" src="/waideek.png" alt="Waideek" width={100} height={80} priority style={{ width: 'auto', height: 'auto' }} />
       </span>
     </a>
 
 
-    <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© Waideek
+    <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">©Waideek
       <a href="https://x.com/sushant_naik_" className="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">@Sushant Naik</a>
     </p>
 
     <span className="sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start flex flex-wrap">
-    {dataset.map((app, index) => {
+    {dataset.map( ( app, index ) => {
       return(
-<Link href={app.link} className="link rounded m-2 hover:text-amber-50 cursor-pointer flex flex-wrap ">
+<Link href={app.link} className="link rounded m-2 hover:text-amber-50 cursor-pointer flex flex-wrap " >
 {app.title}
 </Link>
   )
-    })}
+    } ) }
     </span>
   </div>
 </footer>
